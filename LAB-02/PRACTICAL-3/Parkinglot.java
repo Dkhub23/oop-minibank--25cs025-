abb69d708de576a959a39106cf6cf4f8d2315b6a
Dkhub23 <25cs025@charusat.edu.in>
2026-07-23 22:53:27 +0530

    LAB-02 completed

---
+++ LAB-02/PRACTICAL-3/Parkinglot.java
@@ -0,0 +1,85 @@
+class Parkinglot {
+    private int twoWheelers;
+    private int fourWheelers;
+
+    private final int twoCap;
+    private final int fourCap;
+
+    private static long revenue = 0;
+
+  
+    Parkinglot(int twoCap, int fourCap) {
+        this.twoCap = twoCap;
+        this.fourCap = fourCap;
+        twoWheelers = 0;
+        fourWheelers = 0;
+    }
+
+    
+    void park(String type) {
+        if (type.equalsIgnoreCase("two")) {
+            if (twoWheelers < twoCap) {
+                twoWheelers++;
+                revenue += 20;
+                System.out.println("Two-wheeler parked.");
+            } else {
+                System.out.println("Two-wheeler section Full");
+            }
+        } else if (type.equalsIgnoreCase("four")) {
+            if (fourWheelers < fourCap) {
+                fourWheelers++;
+                revenue += 40;
+                System.out.println("Four-wheeler parked.");
+            } else {
+                System.out.println("Four-wheeler section Full");
+            }
+        }
+    }
+
+   
+    void leave(String type) {
+        if (type.equalsIgnoreCase("two")) {
+            if (twoWheelers > 0) {
+                twoWheelers--;
+                System.out.println("Two-wheeler left.");
+            } else {
+                System.out.println("No two-wheelers to leave.");
+            }
+        } else if (type.equalsIgnoreCase("four")) {
+            if (fourWheelers > 0) {
+                fourWheelers--;
+                System.out.println("Four-wheeler left.");
+            } else {
+                System.out.println("No four-wheelers to leave.");
+            }
+        }
+    }
+
+    
+    void displayStatus() {
+        System.out.println("\nFinal Occupancy:");
+        System.out.println("Two-wheelers : " + twoWheelers);
+        System.out.println("Four-wheelers: " + fourWheelers);
+        System.out.println("Revenue       : ₹" + revenue);
+    }
+
+    public static void main(String[] args) {
+
+        Parkinglot p = new Parkinglot(2, 2);
+
+        p.park("two");
+        p.park("two");
+        p.park("two");     
+        p.park("four");
+        p.park("four");
+        p.park("four");     
+
+        p.leave("two");
+        p.leave("four");
+
+        p.park("two");
+        p.park("four");
+
+        p.displayStatus();
+    }
+}
