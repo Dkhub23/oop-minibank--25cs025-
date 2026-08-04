ea23a7a35592b8984b520a331e4d351417ea6f75
Dkhub23 <25cs025@charusat.edu.in>
2026-08-04 23:13:39 +0530

    LAB-04

--- LAB-04/PRACTICAL-01/Driver.java
+++ LAB-04/PRACTICAL-01/Driver.java
@@ -3,9 +3,11 @@ public class Driver{
         
         String[] pws={
            "welcome",
-           "!@#DHRVi",
+           "!@#DHRUVi",
            "ertgh@W"
         };
+        PasswordChecker.strength(pws[0]);
+        PasswordChecker.strength(pws[1]);
         PasswordChecker.strength(pws[2]);
         
     }
