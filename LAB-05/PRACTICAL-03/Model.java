56281196650a57b6293909e3865f511c7dd07b23
Dkhub23 <25cs025@charusat.edu.in>
2026-08-12 08:25:13 +0530

    LAB-05

--- LAB-05/PRACTICAL-03/Model.java
+++ LAB-05/PRACTICAL-03/Model.java
@@ -1,32 +1,54 @@
 
-abstract class Model {
-   
+abstract class Media {
+    private double latefee;
+    Media(double latefee){
+        this.latefee=latefee;
+    }
 
    abstract double latefee();
     
 }
-class Book extends Model{
+class Book extends Media{
     private double latefee;
     Book(double latefee){
+        super(latefee);
         this.latefee=latefee;
     }
     double latefee(){
         return latefee*20;
     }
 }
-class DVD extends Model{
+class DVD extends Media{
     private double latefee;
-    
-    double latefee(){
-        return latefee*20;
-    }
-}
-class Magazine extends Model{
-    private double latefee;
-    Book(double latefee){
+    DVD(double latefee){
+        super(latefee);
         this.latefee=latefee;
     }
     double latefee(){
         return latefee*20;
+    }
+}
+class Magazine extends Media{
+    private double latefee;
+    Magazine(double latefee){
+        super(latefee);
+        this.latefee=latefee;
+    }
+    double latefee(){
+        return latefee*20;
+    }
+}
+public class Model{
+    public static void main(String[] args) {
+        Media[] m={
+            new Book(3),
+            new DVD(1),
+            new Magazine(5)
+        };
+
+        for(Media i:m){
+            System.out.println("late fee:" +i.latefee()+" for the media:" +i.getClass());
+
+        }
     }
 }
